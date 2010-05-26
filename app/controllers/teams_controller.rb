cd855aea3064a54966152c46b1f8ff584e594360
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-05-26 14:42:37 -0500

    Fixed team controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,32 +1,11 @@
-Fanitoon.controllers :teams do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
+class TeamsController < ApplicationController
 
-  # get :sample, :map => "/sample/url", :respond_to => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
-
-  get :index do
-    @teams = Team.all(:order => 'created_at desc')
-    render 'teams/index'
-    
+  def index
+    @teams = Team.all(:order => 'created_at desc')    
   end
-
-  get :show do
-    @team = Team.find_by_id(params[:id])
-    render 'teams/show'
+  
+  def show
+    @team = Team.find_by_id(params[:id])    
   end
 
 end
