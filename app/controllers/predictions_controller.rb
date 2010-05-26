45ba7bb30dc2c2e6fb75ac883b74b466f2098245
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-05-26 17:49:50 -0500

    Added FB commit

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -28,7 +28,7 @@ class PredictionsController < ApplicationController
       flash[:prediction_notice] = 'Listo campeón. Hemos actualizado tu predicción.'
       # client = reauthenticate(current_account.fb_token)
       client.selection.user(current_account.fb_id).feed.publish!(:message => "¡He pronosticado en Patatoon! Creo que #{@prediction.try(:winner).try(:name)} gana en el partido de #{@prediction.match.local.name} vs #{@prediction.match.try(:visitor).try(:name)} " , :name =>"Ganador: #{@prediction.try(:winner).try(:name)}",:link=>"http://patatoon.heroku.com#{match_path(@prediction.match)}",:description=>"#{current_account.full_name} ha dejado su pronóstico en Patatoon y ha ganado puntos por eso. ¿No te animas?", :picture => "http://patatoon.heroku.com/images/patatoon.png") if current_account.from_facebook?
-      
+       redirect_to edit_match_prediction_path(@prediction.match,@prediction)
     else
       render 'predictions/edit'
     end
