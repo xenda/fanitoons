be88d55a8fefa7b21d4b3d587ad7bbec4647cbf2
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-06-10 17:31:09 -0500

    Updated database.yml for production server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
 set :patatoon, "patatoon"
-set :repository,  "set your repository location here"
+set :repository,  "git@github.com:xenda/fanitoons.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "nginx"                          # Your HTTP server, Apache/etc
-role :app, "nginx"                          # This may be the same as your `Web` server
-role :db,  "mysql", :primary => true # This is where Rails migrations will run
+set :deploy_to, "/home/patatoon/app/"
 
+
+role :web, "patatoon.com"                          # Your HTTP server, Apache/etc
+role :app, "patatoon.com"                          # This may be the same as your `Web` server
+role :db,  "patatoon.com", :primary => true # This is where Rails migrations will run
+
+set :user, "patatoon"
+set :port, 30000
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
