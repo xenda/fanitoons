164e77740d834e912967140d6bb87f2f1b4c776c
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-06-11 06:40:04 -0500

    Added most prediction mark up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     @posts = Post.all(:order => 'created_at desc')
     @matches = Match.all(:order=> "played_at", :limit=>7, :include => [:local, :visitor])
     @predictions = Prediction.last(6).find(:all, :include => [:winner, {:match=>[:local,:visitor]}])
-    @popular_predictions = Match.most_popular(6).find(:all, :include => [:local, :visitor])
+    @popular_predictions = Match.most_popular(4).find(:all, :include => [:local, :visitor])
     @last_match = @matches.first
     @matches = @matches[1..-1]
   end
