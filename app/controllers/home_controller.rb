a491367c53234e2487473ce937bb861b32fb90f6
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-06-12 10:02:51 -0500

    Fixed latest match. Should only change when a couple of hours have passed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   
   def index
     @posts = Post.all(:order => 'published_at desc, created_at desc', :conditions=>["(published_at <= ?) or published_at is null",Time.zone.now])
-    @matches = Match.all(:order=> "played_at", :limit=>7, :include => [:local, :visitor], :conditions => ["played_at >= ?",Time.zone.now])
+    @matches = Match.all(:order=> "played_at", :limit=>7, :include => [:local, :visitor], :conditions => ["played_at >= ?",Time.zone.now - 2.hours])
     @predictions = Prediction.last(6).find(:all, :include => [:winner, {:match=>[:local,:visitor]}])
     @popular_predictions = Match.most_popular(4).find(:all, :include => [:local, :visitor])
     @last_match = @matches.first
