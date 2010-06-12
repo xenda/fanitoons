b989aa36a1bcfe503ea8bd108c80ab0c0cfc5860
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-06-12 10:44:55 -0500

    Revalidated comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :account, :class_name => "Account", :foreign_key => "user_id"
   
-  validates_presence_of :post_id
+  validates_presence_of :commentable_id
   validates_presence_of :content
   validates_presence_of :email
   
