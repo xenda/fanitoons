aed12f5e4b56036443a5e631fb21a213631dabab
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-06-14 05:33:58 -0500

    Updated female

--- public/assets/swf/update_data.rb
+++ public/assets/swf/update_data.rb
@@ -2,28 +2,28 @@ require 'rubygems'
 files = `ls`
 # puts files.split("\n").inspect
 
-shirts = files.select {|file| file=~/shirt/ }
-shorts = files.select {|file| file=~/short/ }
-snickers = files.select {|file| file=~/snicker/ }
+shirts = files.select {|file| file=~/shirt_1_f/ }
+shorts = files.select {|file| file=~/short_1_f/ }
+snickers = files.select {|file| file=~/snicker_1_f/ }
 
 xml_file = "<root>"
 xml_file << "<group_shirt>"
 shirts.each_with_index do |shirt,i|
   name = shirt.split("_").first
-  xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
+  xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
 end
 
 xml_file << "</group_shirt>"
 xml_file << "<group_shorts>"
 shorts.each_with_index do |shirt,i|
   name = shirt.split("_").first
-  xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
+  xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
 end
 xml_file << "</group_shorts>"
 xml_file << "<group_snickers>"
 snickers.each_with_index do |shirt,i|
   name = shirt.split("_").first
-  xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
+  xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
 end
 xml_file << "</group_snickers>"
 
