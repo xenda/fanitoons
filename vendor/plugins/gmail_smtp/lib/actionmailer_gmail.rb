6fe84650adb827cd059d91aa79a679eea2696d2b
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-06-15 07:04:04 -0500

    Updated GMAIL data

--- vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
+++ vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
@@ -1,7 +1,13 @@
+require "openssl"
+require "net/smtp"
+
+ActionMailer::Base.delivery_method = :smtp
+
 ActionMailer::Base.smtp_settings = {
 	:address => "smtp.gmail.com",
 	:port => 587,
 	:authentication => :plain,
+   :enable_starttls_auto => true
 	:domain => ENV['GMAIL_SMTP_USER'],
 	:user_name => ENV['GMAIL_SMTP_USER'],
 	:password => ENV['GMAIL_SMTP_PASSWORD'],
