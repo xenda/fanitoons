5a0c6e5edd386636adf901f420920d8701eb33b6
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-06-18 00:42:08 -0500

    Added comments to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
 
   belongs_to :folder
-
+  has_many :comments, :as=> :commentable
   belongs_to :from, :class_name => "Account", :foreign_key => "from_account_id"
   belongs_to :to,   :class_name => "Account", :foreign_key => "to_account_id"
   
