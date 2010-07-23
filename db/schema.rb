abc23dd8eeb55d9734dd0d1a4e62bb60c6dc3bb1
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-07-23 11:42:34 -0500

    Removed column country from database

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100716180323) do
+ActiveRecord::Schema.define(:version => 20100723164034) do
 
   create_table "abuses", :force => true do |t|
     t.string   "email"
@@ -371,7 +371,6 @@ ActiveRecord::Schema.define(:version => 20100716180323) do
 
   create_table "teams", :force => true do |t|
     t.string   "name"
-    t.string   "country"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "bio"
