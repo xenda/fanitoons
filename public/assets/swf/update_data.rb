621f4779b93be5cdb09036f08a0e2f0360b7edde
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-07-26 20:39:47 -0500

    Updated data

--- public/assets/swf/update_data.rb
+++ public/assets/swf/update_data.rb
@@ -6,24 +6,34 @@ shirts_m = files.select {|file| file=~/shirt_1_m/ }
 shorts_m = files.select {|file| file=~/short_1_m/ }
 snickers_m = files.select {|file| file=~/snickers_1_m/ }
 shoes_m = files.select {|file| file=~/shoes_1_m/ }
-heads_m = files.select {|file| file=~/heads_1_m/ }
+heads_m = files.select {|file| file=~/head_1_m/ }
 
 shirts_f = files.select {|file| file=~/shirt_1_f/ }
 shorts_f = files.select {|file| file=~/short_1_f/ }
 snickers_f = files.select {|file| file=~/snickers_1_f/ }
 shoes_f = files.select {|file| file=~/shoes_1_f/ }
-heads_f = files.select {|file| file=~/heads_1_f/ }
+heads_f = files.select {|file| file=~/head_1_f/ }
+
+def clean(name)
+  
+  [/club_/, /_1_[mf]/, ".swf", "shirt", "short","snickers","head","shoes" ].each do |token| 
+    name = name.gsub(token,"")
+  end
+  name = name.gsub("-","_")
+  name = name.gsub("_", " ")
+  name.strip!
+end
 
 xml_file = "<root>"
 xml_file << "<group_shirt id=\"shirt\">"
 i = 0
 shirts_f.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
   i+=1
 end
 shirts_m.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
   i+=1
 end
@@ -32,12 +42,12 @@ xml_file << "</group_shirt>"
 xml_file << "<group_shorts id=\"shorts\">"
 i = 0
 shorts_f.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
   i+=1
 end
 shorts_m.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
   i+=1
 end
@@ -46,12 +56,12 @@ xml_file << "</group_shorts>"
 xml_file << "<group_snickers id=\"snickers\">"
 i = 0
 snickers_f.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
   i+=1
 end
 snickers_m.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
   i+=1
 end
@@ -60,12 +70,12 @@ xml_file << "</group_snickers>"
 xml_file << "<group_shoes id=\"shoes\">"
 i = 0
 shoes_f.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
   i+=1
 end
 shoes_m.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
   i+=1
 end
@@ -74,12 +84,12 @@ xml_file << "</group_shoes>"
 xml_file << "<group_head id=\"head\">"
 i = 0
 heads_f.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>1</sex></item>"
   i+=1
 end
 heads_m.each do |shirt|
-  name = shirt.split("_").first
+  name = clean(shirt)
   xml_file << "<item><id>#{i+1}</id><name>#{name}</name><picture_file_name>#{shirt}</picture_file_name><sex>0</sex></item>"
   i+=1
 end
